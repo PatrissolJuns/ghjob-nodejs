7ab236d79f90aaa427ef042d8e57b6434475e590
PatrissolJuns <patrissolkenfack@gmail.com>
2021-02-16 17:33:14 +0000

    Add an initializer to run before lauching the main project

---
+++ initializer.js
@@ -0,0 +1,10 @@
+const { jobsDB } = require('./databases');
+const logger = require('./config/logger');
+const { populate } = require('./functions');
+
+// Populate the job database
+logger.info("Population has started");
+populate(jobsDB)
+    .then(() => logger.info("Population has finished"))
+    .catch(error => logger.info("Error while populating " + error));
+
