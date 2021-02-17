7365259bdccb658c75251f28b08291a60ec0eb13
PatrissolJuns <patrissolkenfack@gmail.com>
2021-02-17 18:01:49 +0100

    Updated functions to handle MongoDB instead

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const Job = require('./models/Job');
 const BASE = 'https://jobs.github.com/';
 
 /**
@@ -15,33 +16,31 @@ const getAllJobs = (page = 1) => {
 
 /**
  * Fill in database
- * @param db
  * @param page
  * @param data
- * @returns {Promise<void>}
+ * @returns {Promise<Document<any>>}
  */
-const populate = async (db, page = 1, data = []) => {
+const populate = async (page = 1, data = []) => {
     try {
         let pageData = await getAllJobs(page);
         pageData = pageData.map(job => ({...job, createdAt: new Date(job.created_at).getTime()}));
 
         if (pageData.length > 0) {
-            await populate(db, page + 1, [ ...pageData, ...data]);
-        } else {
-            db.insert(data, function (err, newDocs) {});
-        }
-    } catch (e) {}
+            return await populate(page + 1, [ ...pageData, ...data]);
+        } else return Job.insertMany(data);
+    } catch (e) {
+        return Promise.reject(e);
+    }
 };
 
 /**
  * Check if given jobs are in the database
- * @param db
  * @param ids array of Job's id
  * @returns {Promise<any>}
  */
-const matchingIdItems = (db, ids) => {
+const matchingIdItems = (ids) => {
     return new Promise((resolve, reject) => {
-        db.find({ id: { $in: ids } }, function (err, docs) {
+        Job.find({ id: { $in: ids } }, function (err, docs) {
             return err ? reject(null) : resolve(docs);
         });
     });
@@ -49,47 +48,47 @@ const matchingIdItems = (db, ids) => {
 
 /**
  * Get new jobs
- * @param db
  * @returns {Promise<*>}
  */
-const getNewJobs = async (db) => {
-    return new Promise(async (resolve, reject) => {
-        const newJobs = [];
-        const _getNewJobs = async (db, page = 1, newJobs = []) => {
-            const pageData = await getAllJobs(page);
-            // Stop the recursion in case there's no more data
-            if (pageData.length === 0) {
-                return resolve(newJobs);
-            }
+const getNewJobs = async (page = 1, newJobs = []) => {
+    let pageData, _matchingIdItems;
+    try {
+        pageData = await getAllJobs(page);
+    } catch (e) {
+        return Promise.reject(e);
+    }
 
-            // Get jobs that are in the database
-            const _matchingIdItems = await matchingIdItems(db, pageData.map(i => i.id));
+    // Stop the recursion in case there's no more data
+    if (pageData.length === 0) {
+        return Promise.resolve(newJobs);
+    }
 
-            // Get new one i.e those that are not in the database
-            const _newJobs = pageData.filter(j => _matchingIdItems.find(job => job.id === j.id) === undefined);
+    // Get jobs that are in the database
+    try {
+        _matchingIdItems = await matchingIdItems(pageData.map(i => i.id));
+    } catch (e) {
+        return Promise.reject(e);
+    }
 
-            // Perform another fetch while saving new jobs
-            return _getNewJobs(db, page + 1, [...newJobs, ..._newJobs]);
-        };
-        await _getNewJobs(db, 1, newJobs);
-        resolve(newJobs);
-    })
+    // Get new one i.e those that are not in the database
+    const _newJobs = pageData.filter(j => _matchingIdItems.find(job => job.id === j.id) === undefined);
+
+    // Perform another fetch while saving new jobs
+    return await getNewJobs(page + 1, [...newJobs, ..._newJobs]);
 };
 
 /**
  * Save a given jobs into the database
- * @param db
  * @param jobs
  * @returns {Promise<any>}
  */
-const saveNewJobs = (db, jobs) => {
+const saveNewJobs = (jobs) => {
     return new Promise((resolve, reject) => {
         const _jobs = jobs.map(job => ({...job, createdAt: new Date(job.created_at).getTime()}));
-        db.insert(_jobs, function (err, newDocs) {
-            if (err) return reject(err);
-            return resolve(newDocs);
-        });
-    })
+        Job.insertMany(_jobs)
+            .then((docs) => resolve(docs))
+            .catch((error) => reject(error));
+    });
 };
 
 module.exports = {
